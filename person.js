25dc081682a4d4c7fed978ec35e5eb60b1a26a11
cole92 <aleksandar9212@gmail.com>
2024-09-03 16:59:08 +0200

    Created a module person.js and added a Person class, added a JSON file to create a free API, added an image, and worked on CSS to better focus on JavaScript code and reduce time spent on CSS.

---
+++ person.js
@@ -0,0 +1,9 @@
+export default class Person {
+    constructor(first, last, education, age, eye) {
+        this.firstName = first;
+        this.lastName = last;
+        this.education = education;
+        this.age = age;
+        this.eyeColor = eye;
+    }
+}
